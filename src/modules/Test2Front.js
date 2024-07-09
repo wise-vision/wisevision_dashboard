589ebf2e63f28db852c98ef5bff6cc9e3bbeaa14
Adam Krawczyk <adam.krawczyk@robotec.ai>
2024-07-09 17:29:26 +0200

    Clear
    Signed-off-by: Adam Krawczyk <adam.krawczyk@robotec.ai>

--- src/modules/Test2Front.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Test2Front() {
-  return <div>This is the Test2Front component!</div>;
-}
-
-export default Test2Front;
--- src/modules/TestFront.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function TestFront() {
-  return <div>This is the TestFront component!</div>;
-}
-
-export default TestFront;
