43abcb188506369cd15ad0eac86bb1ab581ad498
Adam Krawczyk <adam.krawczyk@robotec.ai>
2024-08-06 23:21:31 +0200

    Extend api calls
    Signed-off-by: Adam Krawczyk <adam.krawczyk@robotec.ai>

--- app/server/data_object/message_objects.py
+++ app/server/data_object/message_objects.py
@@ -8,21 +8,36 @@ class ROS2Topic:
     def __str__(self):
         return f"{self.name} - {self.type}"
 
+class ROS2Service:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def __str__(self):
+        return f"{self.name} - {self.type}"
+
 class ROS2Topics:
     def __init__(self):
-        self.topics = []  # Instance variable to store topics
+        self.topics = []
 
     def add_topic(self, topic):
-        """Add a topic to the list."""
         self.topics.append(topic)
 
-    def remove_topic(self, topic):
-        """Remove a topic from the list."""
-        self.topics.remove(topic)
-
     def get_topics(self):
-        """Return a list of all topics."""
         return self.topics
 
     def __str__(self):
         return "\n".join(str(topic) for topic in self.topics)
+
+class ROS2Services:
+    def __init__(self):
+        self.services = []
+
+    def add_service(self, service):
+        self.services.append(service)
+
+    def get_services(self):
+        return self.services
+
+    def __str__(self):
+        return "\n".join(str(service) for service in self.services)
