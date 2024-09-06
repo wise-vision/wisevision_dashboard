34019ceae5e7b05ec6050defa222950407fcb0b1
czarekk215 <czarekmadziewczyne@gmail.com>
2024-09-06 08:52:10 +0200

    Integration with ifnluxdb

--- app/server/service/ros2_manager.py
+++ app/server/service/ros2_manager.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.serialization import deserialize_message
 from ..data_object.message_objects import ROS2Topic, ROS2Topics, ROS2Service, ROS2Services
 import importlib
+from dateutil import parser  
 from rosidl_runtime_py.utilities import get_message, get_service
 # from rosidl_runtime_py.utilities import get_message_names_and_types, get_service_names_and_types
 from rclpy.qos import QoSProfile
@@ -69,7 +71,150 @@ class ROS2Manager:
         response = future.result()
 
         return response.success if response else False
+    
+    def call_get_messages_service(self, params):
+        service_type = get_service('lora_msgs/srv/GetMessages')
+        if not service_type:
+            raise ImportError("Service type not found for 'GetMessages'")
+        
+        client = self.node.create_client(service_type, '/get_messages')
+        while not client.wait_for_service(timeout_sec=1.0):
+            if not rclpy.ok():
+                raise Exception("Interrupted while waiting for the service. ROS shutdown.")
 
+
+        request = service_type.Request(**params)
+
+        future = client.call_async(request)
+        rclpy.spin_until_future_complete(self.node, future)
+        response = future.result()
+
+        if response:
+            return {
+                'int32_msgs': response.int32_msgs,
+                'micro_publisher_data': response.micro_publisher_data,
+                'timestamps': response.timestamps
+            }
+        else:
+            return None
+
+
+    def call_get_last_message_service(self, params):
+        
+        service_type = get_service('lora_msgs/srv/GetMessages')
+        if not service_type:
+            raise ImportError("Service type not found for 'GetMessages'")
+
+        client = self.node.create_client(service_type, '/get_messages')
+        while not client.wait_for_service(timeout_sec=1.0):
+            if not rclpy.ok():
+                raise Exception("Interrupted while waiting for the service. ROS shutdown.")
+
+        request = service_type.Request()
+        request.topic_name = params.get('topic_name')
+        print('topic_name:', request.topic_name)
+        request.message_type = params.get('message_type')
+        print('message_type:', request.message_type)
+        request.number_of_msgs = 1
+
+        future = client.call_async(request)
+        print('params:')
+        rclpy.spin_until_future_complete(self.node, future)
+        response = future.result()
+        
+        if response:
+            return {
+                'int32_msgs': response.int32_msgs,
+                'micro_publisher_data': response.micro_publisher_data,
+                'timestamps': response.timestamps
+            }
+        else:
+            self.get_logger().error('Error while retrieving messages from the service.')
+            return None  
+
+    def call_get_messages_service_any(self, params):
+        service_type = get_service('lora_msgs/srv/GetMessages')
+        if not service_type:
+            raise ImportError("Service type not found for 'GetMessages'")
+
+        client = self.node.create_client(service_type, '/get_messages')
+        while not client.wait_for_service(timeout_sec=1.0):
+            if not rclpy.ok():
+                raise Exception("Interrupted while waiting for the service. ROS shutdown.")
+
+        request = service_type.Request()
+        request.topic_name = params.get('topic_name')
+        request.message_type = 'any'
+        request.number_of_msgs = params.get('number_of_msgs', 0)
+
+        def parse_iso8601_to_fulldatetime(iso8601_str):
+            FullDateTime = get_message('lora_msgs/msg/FullDateTime')
+
+            dt = parser.isoparse(iso8601_str)
+
+            full_datetime = FullDateTime()
+            full_datetime.year = dt.year
+            full_datetime.month = dt.month
+            full_datetime.day = dt.day
+            full_datetime.hour = dt.hour
+            full_datetime.minute = dt.minute
+            full_datetime.second = dt.second
+            full_datetime.nanosecond = dt.microsecond * 1000 
+
+            return full_datetime
+
+        if 'time_start' in params:
+            request.time_start = parse_iso8601_to_fulldatetime(params['time_start'])
+        if 'time_end' in params:
+            request.time_end = parse_iso8601_to_fulldatetime(params['time_end'])
+
+
+        future = client.call_async(request)
+        rclpy.spin_until_future_complete(self.node, future)
+        response = future.result()
+
+        if response:
+            try:
+                MessageType = get_message(params.get('message_type'))
+                messages = []
+                data = response.data
+                offset = 0
+
+                while offset < len(data):
+                    message_length = int.from_bytes(data[offset:offset + 4], byteorder='big')
+                    offset += 4
+
+                    message_data = bytes(data[offset:offset + message_length])
+                    offset += message_length
+
+                    message = deserialize_message(message_data, MessageType())
+                    messages.append(message)
+
+                def serialize_ros_message(msg):
+                    result = {}
+                    for field_name, field_type in msg.get_fields_and_field_types().items():
+                        value = getattr(msg, field_name)
+                        if hasattr(value, 'get_fields_and_field_types'):
+                            result[field_name] = serialize_ros_message(value)
+                        elif isinstance(value, list):
+                            result[field_name] = [
+                                serialize_ros_message(v) if hasattr(v, 'get_fields_and_field_types') else v
+                                for v in value
+                            ]
+                        else:
+                            result[field_name] = value
+                    return result
+
+                serialized_response = {
+                    'timestamps': [serialize_ros_message(timestamp) for timestamp in response.timestamps],
+                    'messages': [serialize_ros_message(msg) for msg in messages]
+                }
+                return serialized_response
+            except Exception as e:
+                raise Exception(f"Message deserialization error:” {e}")
+        else:
+            return None
+       
     def shutdown(self):
         rclpy.shutdown()
 
