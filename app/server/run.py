2809b3501e9f15f1b5d620e970a30a2147b25908
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-01-23 18:31:55 +0100

    Add license

--- app/server/run.py
+++ app/server/run.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+#
+#  Copyright (C) 2025 wisevision
+#
+#  SPDX-License-Identifier: MPL-2.0
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
 
 from . import create_app, socketio
 import sys
